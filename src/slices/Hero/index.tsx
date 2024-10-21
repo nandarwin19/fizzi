3e4d96f2ed2a5a4630fa7c007699f5668b3d3b08
nwin19 <nandarwinbuma@gmail.com>
2024-10-21 12:55:52 +0630

    feat: Handling the mobile version

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -13,6 +13,7 @@ import { View } from "@react-three/drei";
 import Scene from "./Scene";
 import { Bubbles } from "./Bubbles";
 import { useStore } from "@/app/hooks/useStore";
+import { useMediaQuery } from "@/app/hooks/useMediaQuery";
 
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
@@ -27,9 +28,11 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   const ready = useStore((state) => state.ready);
+  const isDesktop = useMediaQuery("(min-width: 768px)", true);
+
   useGSAP(
     () => {
-      if (!ready) return;
+      if (!ready && isDesktop) return;
 
       const introTl = gsap.timeline();
 
@@ -105,10 +108,12 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="hero opacity-0"
     >
-      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
-        <Scene/>
-        <Bubbles count={300} speed={2} repeat={true}/>
-      </View>
+       {isDesktop && (
+        <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+          <Scene />
+          <Bubbles count={300} speed={2} repeat={true} />
+        </View>
+      )}
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
